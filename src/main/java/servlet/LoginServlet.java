dd4517b5cf5bb2dfdbd8028b484fca5e5b9e8582
ryota <ryota@PC>
2025-11-06 14:41:26 +0900

    a

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -13,10 +13,6 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
--- src/main/java/servlet/UserRegisterServlet.java
+++ src/main/java/servlet/UserRegisterServlet.java
@@ -13,11 +13,6 @@ import jakarta.servlet.http.HttpServletResponse;
 public class UserRegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
